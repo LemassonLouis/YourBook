e6cf1b0bd07510765ca4180f6c9682a98d223705
LEMASSON Louis <galou2608@gmail.com>
2023-03-20 20:19:51 +0100

    Adding navigation

--- frameworkjs/app/root.jsx
+++ frameworkjs/app/root.jsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import MainNavigation from "./components/MainNavigation";
+
 import styles from '~/styles/main.css';
 
 export const meta = () => ({
@@ -23,6 +25,9 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
