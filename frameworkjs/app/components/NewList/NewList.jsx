c5f4a840511825a5be5019e1b1884317c4c30e1f
LEMASSON Louis <galou2608@gmail.com>
2023-05-02 10:53:45 +0200

    NewList component pt.3

--- frameworkjs/app/components/NewList/NewList.jsx
+++ frameworkjs/app/components/NewList/NewList.jsx
@@ -2,29 +2,14 @@ import { Form } from '@remix-run/react';
 import { createBookList } from '../../services/book_list';
 
 import styles from './NewList.css';
+import { redirect } from '@remix-run/node';
+import { loggedUser } from '../../services/auth.server';
 
 export default function NewList() {
-
-  function toggleModal(event) {
-
-    // Get modal
-    const parent = event.target.parentElement;
-    const modal = parent.querySelector("dialog");
-
-    // toggle modal display
-    if(modal.open) modal.close();
-    else {
-      modal.show();
-      modal.addEventListener("click", (event) => {
-        if(event.target == modal) modal.close();
-      });
-    }
-  }
-
   return (
     <div className='NewList'>
       <button className='CTA-button' onClick={toggleModal}>Créer une nouvelle liste</button>
-      <dialog className='modal-NewList'>
+      <dialog className='modal-NewList' onBlur={() => console.log("onblur")} onFocus={(e) => console.log("onfocusout", e)}>
         <Form method='post'>
           <input type='text' className="CTA-input" name='name' placeholder='Nom de la liste' required></input>
           <button className='CTA-button'>Créer</button>
@@ -43,11 +28,31 @@ export function links() {
   ]
 }
 
+export function toggleModal(event) {
+
+  // Get modal
+  const parent = event.target.parentElement;
+  const modal = parent.querySelector("dialog");
+
+  // toggle modal display
+  if(modal.open) modal.close();
+  else {
+    modal.show();
+    // modal.addEventListener("click", (event) => {
+    //   if(event.target == modal) modal.close();
+    // });
+  }
+}
+
 export async function newBookList(request) {
+
+  const user = await loggedUser(request);
+  if(!user) redirect('/register');
 
   const formData = await request.formData();
   const bookListData = {
     name: formData.get("name"),
+    ownerId: user.id
   }
 
   await createBookList(bookListData);
